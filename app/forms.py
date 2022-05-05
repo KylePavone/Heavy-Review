29e0501b8dc903fadcd0a399d77a3a580a60c43f
dima <scaryfase1237373>
2022-05-05 20:35:16 +0300

    "added feature 'forgot password' and some visual changes"

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,8 @@ class RegisterForm(FlaskForm):
     name = StringField('username', [InputRequired(), Length(min=5, max=25)])
     email = StringField('email', validators=[InputRequired(), Length(min=5, max=150)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=150)])
+
+
+class ChangepPassForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Length(min=5, max=150)])
+    new_password = PasswordField('Новый пароль', validators=[InputRequired(), Length(min=5, max=150)])
--- app/view.py
+++ app/view.py
@@ -1,6 +1,6 @@
 from app import app, db, user_datastore
-from flask import render_template, request, redirect, session
-from forms import RegisterForm
+from flask import render_template, request, redirect, session, url_for
+from forms import RegisterForm, ChangepPassForm
 from datetime import timedelta
 from models import User, Role
 
@@ -15,6 +15,11 @@ def make_session_permanent():
 def base():
     return render_template('base.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -26,3 +31,22 @@ def register():
             db.session.commit()
         return redirect('/login')
     return render_template('register.html', form=form)
+
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    form = ChangepPassForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                user = User.query.filter(User.email == form.email.data).first()
+                user.password = form.new_password.data
+                db.session.commit()
+            except:
+                return "Wrong email!"
+        return redirect(url_for('security.login'))
+    return render_template('forgot_password.html', form=form)
+
+
+
+
